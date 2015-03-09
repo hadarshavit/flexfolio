318167d6808444c8ad08bc17a71d98165564ec71
Marius Lindauer <marius.rks@googlemail.com>
2015-03-09 16:00:26 +0100

    removed satzilla data

--- src/treefolio/scripts/callerScript_feats2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Call: callerScript.sh [instance] [features]
-
-if [[ -f $1 ]]; then
-if [[ -f $2 ]]; then
-	binaries/gringo $1 | binaries/claspre > $2
-fi
-fi
--- src/treefolio/scripts/executionScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Call: executionScript.sh [instance]
-
-if [[ -f $1 ]]; then
-	binaries/gringo $1 | binaries/clasp
-fi
